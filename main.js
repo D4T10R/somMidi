d3ccedb9d61cc54b979915fc8f2f3616ab6561c3
Datior <leonardowederveiga@yahoo.com>
2023-02-22 21:05:57 -0300

    Iteração con lista e parametros JS

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(IdElementoSom) {
+    document.querySelector(IdElementoSom).play();
 }
 
-document.querySelector('tecla_clap');
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let contador = 0; contador < listaDeTeclas.length; contador ++){
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    tecla.onclick = function() {
+        tocaSom(`#som_${instrumento}`);
+    }
+
+}
